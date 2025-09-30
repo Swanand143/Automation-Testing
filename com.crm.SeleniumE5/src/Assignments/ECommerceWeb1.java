458c41b2aeab2acdbd1d0293928e9792c57f0a1c
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-09-30 13:32:24 +0530

    Assignment Added

---
+++ com.crm.SeleniumE5/src/Assignments/ECommerceWeb1.java
@@ -0,0 +1,16 @@
+package Assignments;
+
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class ECommerceWeb1 {
+	
+	public static void main(String[] args) {
+		
+		EdgeDriver edge = new EdgeDriver();
+		edge.manage().window().maximize();
+		edge.get("https://www.amazon.in/");
+		edge.quit();
+		
+	}
+
+}
---
+++ com.crm.SeleniumE5/src/Assignments/ECommerceWeb2.java
@@ -0,0 +1,16 @@
+package Assignments;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ECommerceWeb2 {
+	
+	public static void main(String[] args) {
+		
+		FirefoxDriver fire = new FirefoxDriver();
+		fire.manage().window().maximize();
+		fire.get("https://www.flipkart.com/");
+		fire.close();
+		
+	}
+
+}
