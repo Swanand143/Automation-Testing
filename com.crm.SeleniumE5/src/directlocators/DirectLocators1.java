d2f81a34e05ab3afa5511e0ba60a1d9062e49a4f
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-09-30 13:31:32 +0530

    Direct Locators Programms

---
+++ com.crm.SeleniumE5/src/directlocators/DirectLocators1.java
@@ -0,0 +1,27 @@
+package directlocators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DirectLocators1 {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com");
+		//by tagname
+		WebElement searchfield = driver.findElement(By.tagName("input"));
+		searchfield.sendKeys("Playstation 5");
+//		WebElement searchfield = driver.findElement(By.tagName("a"));
+		
+		//by classname
+	    //driver.findElement(By.className("ico-register")).click();
+		driver.findElement(By.className("button-1")).click();
+		Thread.sleep(3000);
+		driver.close();
+			
+	}
+
+}
