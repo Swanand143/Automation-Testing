9625ce092c88d700b9bcd2efabd7c3e6a2e4e603
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-10-22 20:05:50 +0530

    Multiple Select DropDown

---
+++ com.crm.SeleniumE5/src/selectClass/MultiSelectDropDown.java
@@ -0,0 +1,32 @@
+package selectClass;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class MultiSelectDropDown {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///D:/Chrome/demo.html");
+		Thread.sleep(2000);
+		
+		WebElement multiSelect = driver.findElement(By.id("multiple_cars"));
+		
+		Select sel = new Select(multiSelect);
+		sel.selectByIndex(1);
+		Thread.sleep(2000);
+		
+		sel.selectByVisibleText("BMW");
+		Thread.sleep(2000);
+		
+		sel.selectByValue("for");
+		Thread.sleep(5000);
+		
+		driver.quit();		
+	}
+}
