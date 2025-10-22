553d7f0c486161443f108c99fcf8d09776c09f08
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-10-22 20:05:18 +0530

    Singe Select DropDown

---
+++ com.crm.SeleniumE5/src/selectClass/SingleSelectDropDown.java
@@ -0,0 +1,34 @@
+package selectClass;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+
+public class SingleSelectDropDown {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///D:/Chrome/demo.html");
+		Thread.sleep(2000);
+		
+		WebElement singleSelect = driver.findElement(By.id("standard_cars"));
+		
+		Select sel = new Select(singleSelect);
+		sel.selectByIndex(2);
+		Thread.sleep(2000);
+		
+		sel.selectByVisibleText("Jaguar");
+		Thread.sleep(2000);
+		
+		sel.selectByValue("vol");
+		Thread.sleep(5000);
+		
+		driver.quit();	
+	}
+
+}
