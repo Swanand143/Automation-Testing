a986c12359c4ac8d382521637430ed370251f8ef
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-11-15 23:09:34 +0530

    Mouse Scrolling To The Element

---
+++ com.crm.SeleniumE5/src/ActionsClass/MouseScrollingToElement.java
@@ -0,0 +1,29 @@
+package ActionsClass;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class MouseScrollingToElement {
+	
+public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com/");
+		Thread.sleep(1000);
+		
+		Actions act = new Actions(driver);
+		WebElement headline = driver.findElement(By.xpath("//h3[text()='My account']"));
+		
+		//scrolling based on the web element
+		act.scrollToElement(headline).perform();
+		
+		Thread.sleep(3000);
+		driver.quit();
+		
+	}
+
+}
