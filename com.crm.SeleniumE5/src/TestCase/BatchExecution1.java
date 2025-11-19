1d821247361055377eda736754a85684a21e9afd
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-11-19 23:20:12 +0530

    Batch execution code

---
+++ com.crm.SeleniumE5/src/TestCase/BatchExecution1.java
@@ -0,0 +1,11 @@
+package TestCase;
+
+public class BatchExecution1 {
+	
+	public static void main(String[] args) throws Exception {
+		HomePage1.searchField();
+		HomePage1.checkProductAdded();
+		HomePage1.registerDisplay();
+	}
+
+}
