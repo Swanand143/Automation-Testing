62eff5a5e37e928dd15e701bffe14498627a4497
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-11-23 22:09:10 +0530

    Direct Locator 4

---
+++ com.crm.SeleniumE5/src/locators/DirectLocators4.java
@@ -0,0 +1,24 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DirectLocators4 {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Thread.sleep(1000);
+		driver.get("https://demowebshop.tricentis.com");
+		Thread.sleep(1000);
+		driver.findElement(By.className("ico-register")).click();
+	    Thread.sleep(2000);
+	    driver.findElement(By.id("gender-male")).click();
+	    Thread.sleep(3000);
+	    driver.quit();
+		
+		
+	}
+
+}
