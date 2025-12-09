21c6318af008fe58f2d2c46f64beede737eea9d6
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-12-09 20:52:16 +0530

    Read From XML File Program

---
+++ com.crm.AdvanceSeleniumPro/src/test/java/Parameterization/ReadFromXML.java
@@ -0,0 +1,28 @@
+package Parameterization;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ReadFromXML {
+	
+	@Parameters({"url","username","password"})
+	@Test
+	public void readXml(String url, String username, String password) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get(url);
+		driver.findElement(By.name("username")).sendKeys(username);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		Thread.sleep(4000);
+		driver.quit();
+	}
+	
+
+}
