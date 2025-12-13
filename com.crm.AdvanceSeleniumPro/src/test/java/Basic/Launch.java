975cb14c23b03de79471d0022d6f1f35950f9615
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-12-13 21:20:29 +0530

    Basic Lunch Test Program

---
+++ com.crm.AdvanceSeleniumPro/src/test/java/Basic/Launch.java
@@ -0,0 +1,43 @@
+package Basic;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Launch {
+	
+	@Test
+	public void Mi() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("https://www.mumbaiindians.com/");
+		driver.quit();
+		
+	}
+	
+	@Test
+	public void csk() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("https://www.chennaisuperkings.com/");
+		driver.quit();
+		
+	}
+	
+	@Test
+	public void rcb() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("https://www.royalchallengers.com/");
+		driver.quit();
+		
+	}
+}
