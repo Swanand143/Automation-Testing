606081c56d52f48cec51b163b4fa9cf3c9a8572c
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-12-15 20:50:15 +0530

    In Helper Method Innvocation Program Added

---
+++ com.crm.AdvanceSeleniumPro/src/test/java/HelperMethods/InvocationCount.java
@@ -0,0 +1,45 @@
+package HelperMethods;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class InvocationCount {
+	
+	@Test(priority = 1)
+	public void Mi() {
+		
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("https://www.mumbaiindians.com/");
+		driver.quit();
+		
+	}
+	
+	@Test(priority = 2)
+	public void csk() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("https://www.chennaisuperkings.com/");
+		driver.quit();
+		
+	}
+	
+	@Test(priority = 0, invocationCount = 3)
+	public void rcb() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		driver.get("https://www.royalchallengers.com/");
+		driver.quit();
+		
+	}
+
+}
