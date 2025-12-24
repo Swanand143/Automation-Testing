ae9c29dd9f7fa5fd78f12f2b9457684f9478fc2b
Swanand Gholap <156060301+Swanand143@users.noreply.github.com>
2025-12-24 20:51:22 +0530

    Simple Listener Of ITestlistener Program

---
+++ com.crm.AdvanceSeleniumPro/src/main/java/com/crm/Listeners/Simple.java
@@ -0,0 +1,39 @@
+package com.crm.Listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+public class Simple implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		Reporter.log("onTestStart",true);
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		Reporter.log("onTestSuccess",true);
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		Reporter.log("onTestFailure",true);
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		Reporter.log("onTestSkipped",true);
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		Reporter.log("onStart",true);
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		Reporter.log("onFinish",true);
+	}
+}
